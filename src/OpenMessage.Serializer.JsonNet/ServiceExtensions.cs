b87a0a5660db033d515f458b63ab9c081a4afcc8
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2016-07-25 23:23:44 +0100

    Added doc comment

--- src/OpenMessage.Serializer.JsonNet/ServiceExtensions.cs
+++ src/OpenMessage.Serializer.JsonNet/ServiceExtensions.cs
@@ -5,6 +5,9 @@ namespace OpenMessage.Serializer.JsonNet
 {
     public static class ServiceExtensions
     {
+        /// <summary>
+        ///     Adds a Newtonsoft.Json serializer to OpenMessage.
+        /// </summary>
         public static IServiceCollection AddJsonNetSerializer(this IServiceCollection services)
         {
             if (services == null)
