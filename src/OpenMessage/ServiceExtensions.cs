bb5a6e4e15ad5071ee8682daf9349bf3871efa8f
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2016-08-01 22:53:42 +0100

    Removed unneccesary overload

--- src/OpenMessage/ServiceExtensions.cs
+++ src/OpenMessage/ServiceExtensions.cs
@@ -1,23 +1,10 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using System;
 
 namespace OpenMessage
 {
     public static class ServiceExtensions
     {
-        /// <summary>
-        ///     Adds a message broker for the specified type.
-        /// </summary>
-        public static IServiceCollection TryAddBroker<T>(this IServiceCollection services)
-        {
-            if (services == null)
-                throw new ArgumentNullException(nameof(services));
-
-            services.TryAddScoped<IBroker<T>, MessageBroker<T>>();
-            return services;
-        }
-
         /// <summary>
         ///     Creates an observer from the specified action.
         /// </summary>
