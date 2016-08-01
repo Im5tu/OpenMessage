c8e05f8af3b3462c87015b2857f18352f8049ff5
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2016-08-01 21:44:42 +0100

    Extracted out observable for future use

---
+++ src/OpenMessage/ManagedObservable.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace OpenMessage
+{
+    internal abstract class ManagedObservable<T> : IObservable<T>, IDisposable
+    {
+        private readonly ILogger<ManagedObservable<T>> _logger;
+        private readonly HashSet<IObserver<T>> _observers = new HashSet<IObserver<T>>();
+
+        public ManagedObservable(ILogger<ManagedObservable<T>> logger)
+        {
+            if (logger == null)
+                throw new ArgumentNullException(nameof(logger));
+
+            _logger = logger;
+        }
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            if (observer == null)
+                throw new ArgumentNullException(nameof(observer));
+
+            lock (_observers)
+            {
+                _observers.Add(observer);
+                return new Disposable(() =>
+                {
+                    lock (_observers)
+                        if (_observers.Remove(observer))
+                            observer.OnCompleted();
+                });
+            }
+        }
+
+        protected void Notify(T entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            lock (_observers)
+            {
+                var errors = new List<Exception>();
+                foreach (var observer in _observers)
+                    try
+                    {
+                        observer.OnNext(entity);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex.Message, ex);
+                        errors.Add(ex);
+                    }
+
+                if (errors.Count > 0)
+                    throw new AggregateException(errors);
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            lock (_observers)
+            {
+                foreach (var observer in _observers)
+                    observer.OnCompleted();
+
+                _observers.Clear();
+            }
+        }
+    }
+}
--- src/OpenMessage/MessageBroker.cs
+++ src/OpenMessage/MessageBroker.cs
@@ -1,19 +1,19 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace OpenMessage
 {
-    internal sealed class MessageBroker<T> : IBroker<T>
+    internal sealed class MessageBroker<T> : ManagedObservable<T>, IBroker<T>
     {
         private readonly HashSet<IDisposable> _subscriptions = new HashSet<IDisposable>();
         private readonly HashSet<IObservable<T>> _observables = new HashSet<IObservable<T>>();
-        private readonly HashSet<IObserver<T>> _observers;
-        private readonly ILogger<MessageBroker<T>> _logger;
 
         public MessageBroker(IEnumerable<IObserver<T>> observers,
             IEnumerable<IObservable<T>> observables,
-            ILogger<MessageBroker<T>> logger)
+            ILogger<ManagedObservable<T>> logger)
+            : base(logger)
         {
             if (observers == null)
                 throw new ArgumentNullException(nameof(observers));
@@ -21,13 +21,7 @@ namespace OpenMessage
             if (observables == null)
                 throw new ArgumentNullException(nameof(observables));
 
-            if (logger == null)
-                throw new ArgumentNullException(nameof(logger));
-
-            _observers = new HashSet<IObserver<T>>(observers);
-            _logger = logger;
-
-            foreach (var observer in _observers)
+            foreach (var observer in observers)
                 _subscriptions.Add(Subscribe(observer));
 
             foreach (var observable in observables)
@@ -42,60 +36,15 @@ namespace OpenMessage
             }
         }
 
-        public IDisposable Subscribe(IObserver<T> observer)
+        public override void Dispose(bool disposing)
         {
-            if (observer == null)
-                throw new ArgumentNullException(nameof(observer));
+            foreach (var subscription in _subscriptions)
+                subscription.Dispose();
 
-            lock (_observers)
-            {
-                _observers.Add(observer);
-                return new Disposable(() =>
-                {
-                    lock (_observers)
-                        if(_observers.Remove(observer))
-                            observer.OnCompleted();
-                });
-            }
-        }
+            _subscriptions.Clear();
 
-        private void Notify(T entity)
-        {
-            if (entity == null)
-                throw new ArgumentNullException(nameof(entity));
-
-            lock (_observers)
-            {
-                var errors = new List<Exception>();
-                foreach (var observer in _observers)
-                    try
-                    {
-                        observer.OnNext(entity);
-                    }
-                    catch (Exception ex)
-                    {
-                        _logger.LogError(ex.Message, ex);
-                        errors.Add(ex);
-                    }
-
-                if (errors.Count > 0)
-                    throw new AggregateException(errors);
-            }
-        }
-
-        public void Dispose()
-        {
-            lock (_observers)
-            {
-                foreach (var subscription in _subscriptions)
-                    subscription.Dispose();
-
-                foreach (var observer in _observers)
-                    observer.OnCompleted();
-
-                _observers.Clear();
-                _subscriptions.Clear();
-            }
+            foreach (var observable in _observables.OfType<IDisposable>())
+                observable.Dispose();
         }
     }
 }
