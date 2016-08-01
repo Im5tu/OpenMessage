15bddd1f5adafa748caed141138678e6cf333dc7
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2016-08-01 22:12:06 +0100

    Implemented a namespace manager

--- src/OpenMessage.Providers.Azure/Configuration/OpenMessageAzureProviderOptions.cs
+++ src/OpenMessage.Providers.Azure/Configuration/OpenMessageAzureProviderOptions.cs
@@ -8,10 +8,37 @@ namespace OpenMessage.Providers.Azure.Configuration
         ///     The connection string to use when connection to the Azure Service Bus Namespace.
         /// </summary>
         public string ConnectionString { get; set; }
-
         /// <summary>
         ///     The timeout period for operations that occur in Azure.
         /// </summary>
         public TimeSpan RemoteOperationTimeout { get; set; } = TimeSpan.FromSeconds(5);
+        /// <summary>
+        ///     Sets the idle interval after which the queue/topic/subscription is automatically deleted. The minimum duration is 5 minutes.
+        /// </summary>
+        public TimeSpan AutoDeleteOnIdle { get; set; } = TimeSpan.MaxValue;
+        /// <summary>
+        ///     Determines whether or not to enable the queue to be partitioned across multiple message brokers. An express queue holds a message in memory temporarily before writing it to persistent storage.
+        /// </summary>
+        public bool EnableExpress { get; set; } = true;
+        /// <summary>
+        ///     Determines whether or not the queue should be partitioned across multiple message brokers when enabled.
+        /// </summary>
+        public bool EnablePartitioning { get; set; } = true;
+        /// <summary>
+        ///     Determines whether server-side batched operations are enabled.
+        /// </summary>
+        public bool EnableServerSideBatchedOperations { get; set; } = true;
+        /// <summary>
+        ///     Sets the maximum delivery count. A message is automatically deadlettered after this number of deliveries.
+        /// </summary>
+        public int MaximumDeliveryCount { get; set; } = 10;
+        /// <summary>
+        ///     Gets or sets the duration of a peek lock; that is, the amount of time that the message is locked for other receivers. The maximum value for is 5 minutes; the default value is 1 minute.
+        /// </summary>
+        public TimeSpan MessageLockDuration { get; set; } = TimeSpan.FromMinutes(1);
+        /// <summary>
+        ///     Gets or sets the default message time to live value. This is the duration after which the message expires, starting from when the message is sent to Service Bus.
+        /// </summary>
+        public TimeSpan MessageTimeToLive { get; set; } = TimeSpan.MaxValue;
     }
 }
---
+++ src/OpenMessage.Providers.Azure/Management/NamespaceManager.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.ServiceBus.Messaging;
+using OpenMessage.Providers.Azure.Configuration;
+using OpenMessage.Providers.Azure.Conventions;
+using System;
+using System.Threading.Tasks;
+using ServiceBus = Microsoft.ServiceBus.NamespaceManager;
+
+namespace OpenMessage.Providers.Azure.Management
+{
+    public class NamespaceManager<T> : INamespaceManager<T>
+    {
+        private readonly ILogger<NamespaceManager<T>> _logger;
+        private readonly OpenMessageAzureProviderOptions<T> _options;
+        private readonly IQueueNamingConvention _queueNamingConvention;
+        private readonly ISubscriptionNamingConvention _subscriptionNamingConvention;
+        private readonly ITopicNamingConvention _topicNamingConvention;
+
+        public NamespaceManager(IOptions<OpenMessageAzureProviderOptions<T>> options,
+            IQueueNamingConvention queueNamingConvention,
+            ITopicNamingConvention topicNamingConvention,
+            ISubscriptionNamingConvention subscriptionNamingConvention,
+            ILogger<NamespaceManager<T>> logger)
+        {
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            if (queueNamingConvention == null)
+                throw new ArgumentNullException(nameof(queueNamingConvention));
+
+            if (topicNamingConvention == null)
+                throw new ArgumentNullException(nameof(topicNamingConvention));
+
+            if (subscriptionNamingConvention == null)
+                throw new ArgumentNullException(nameof(subscriptionNamingConvention));
+
+            if (logger == null)
+                throw new ArgumentNullException(nameof(logger));
+
+            _options = options.Value;
+            _logger = logger;
+            _queueNamingConvention = queueNamingConvention;
+            _topicNamingConvention = topicNamingConvention;
+            _subscriptionNamingConvention = subscriptionNamingConvention;
+        }
+
+        public async Task ProvisionQueueAsync()
+        {
+            var queueName = _queueNamingConvention.GenerateName<T>();
+
+            var manager = CreateServiceBusManager();
+
+            if (!(await manager.QueueExistsAsync(queueName)))
+            {
+                _logger.LogDebug($"Provisioning queue: '{queueName}'");
+
+                // TODO :: Implement retry policies
+                try
+                {
+                    await manager.CreateQueueAsync(new QueueDescription(queueName)
+                    {
+                        AutoDeleteOnIdle = _options.AutoDeleteOnIdle,
+                        DefaultMessageTimeToLive = _options.MessageTimeToLive,
+                        EnableBatchedOperations = _options.EnableServerSideBatchedOperations,
+                        EnableExpress = _options.EnableExpress,
+                        EnablePartitioning = _options.EnablePartitioning,
+                        LockDuration = _options.MessageLockDuration,
+                        MaxDeliveryCount = _options.MaximumDeliveryCount
+                    });
+
+                    _logger.LogInformation($"Provisioned queue: '{queueName}'");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"An error occured whilst provisioning queue: '{queueName}'", ex);
+                }
+            }
+            else
+                _logger.LogDebug($"Queue '{queueName}' already exists, no need to provision.");
+        }
+
+        public async Task ProvisionSubscriptionAsync()
+        {
+            var topicName = _topicNamingConvention.GenerateName<T>();
+            var subscriptionName = _subscriptionNamingConvention.GenerateName<T>();
+
+            var manager = CreateServiceBusManager();
+
+            await ProvisionTopicAsync(manager, topicName);
+
+            if (!(await manager.SubscriptionExistsAsync(topicName, subscriptionName)))
+            {
+                _logger.LogDebug($"Provisioning subscription: '{topicName}/{subscriptionName}'");
+
+                // TODO :: Implement retry policies
+                try
+                {
+                    await manager.CreateSubscriptionAsync(new SubscriptionDescription(topicName, subscriptionName)
+                    {
+                        AutoDeleteOnIdle = _options.AutoDeleteOnIdle,
+                        DefaultMessageTimeToLive = _options.MessageTimeToLive,
+                        EnableBatchedOperations = _options.EnableServerSideBatchedOperations,
+                        LockDuration = _options.MessageLockDuration,
+                        MaxDeliveryCount = _options.MaximumDeliveryCount
+                    });
+
+                    _logger.LogInformation($"Provisioned subscription: '{topicName}/{subscriptionName}'");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"An error occured whilst provisioning subscription: '{topicName}/{subscriptionName}'", ex);
+                }
+            }
+            else
+                _logger.LogDebug($"Subscription '{topicName}/{subscriptionName}' already exists, no need to provision.");
+        }
+
+        public Task ProvisionTopicAsync() => ProvisionTopicAsync(CreateServiceBusManager(), _topicNamingConvention.GenerateName<T>());
+
+        private async Task ProvisionTopicAsync(ServiceBus manager, string topicName)
+        {
+            if (!(await manager.TopicExistsAsync(topicName)))
+            {
+                _logger.LogDebug($"Provisioning topic: '{topicName}'");
+
+                // TODO :: Implement retry policies
+                try
+                {
+                    await manager.CreateTopicAsync(new TopicDescription(topicName)
+                    {
+                        AutoDeleteOnIdle = _options.AutoDeleteOnIdle,
+                        DefaultMessageTimeToLive = _options.MessageTimeToLive,
+                        EnableBatchedOperations = _options.EnableServerSideBatchedOperations,
+                        EnableExpress = _options.EnableExpress,
+                        EnablePartitioning = _options.EnablePartitioning
+                    });
+
+                    _logger.LogInformation($"Provisioned topic: '{topicName}'");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"An error occured whilst provisioning topic: '{topicName}'", ex);
+                }
+            }
+            else
+                _logger.LogDebug($"Topic '{topicName}' already exists, no need to provision.");
+        }
+
+        private ServiceBus CreateServiceBusManager()
+        {
+            var manager = new ServiceBus(_options.ConnectionString);
+
+            manager.Settings.OperationTimeout = _options.RemoteOperationTimeout;
+
+            return manager;
+        }
+    }
+}
