8808bac173741b38845e4c3a2e50fe08c1eea899
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2016-08-02 21:46:43 +0100

    Implemented dispatchers for queues/topics

---
+++ src/OpenMessage.Providers.Azure/Dispatchers/QueueDispatcher.cs
@@ -0,0 +1,21 @@
+﻿using OpenMessage.Providers.Azure.Management;
+using System;
+using System.Threading.Tasks;
+
+namespace OpenMessage.Providers.Azure.Dispatchers
+{
+    internal sealed class QueueDispatcher<T> : IDispatcher<T>
+    {
+        private readonly IQueueClient<T> _client;
+
+        public QueueDispatcher(IQueueFactory<T> topicFactory)
+        {
+            if (topicFactory == null)
+                throw new ArgumentNullException(nameof(topicFactory));
+
+            _client = topicFactory.Create();
+        }
+
+        public Task DispatchAsync(T entity, TimeSpan scheduleIn) => _client.SendAsync(entity, scheduleIn);
+    }
+}
