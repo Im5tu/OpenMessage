de09c6e31eaea8092af4686c70c80948acd9ac54
Stuart Blackler (@Im5tu) <Im5tu@users.noreply.github.com>
2016-10-15 21:19:20 +0100

    Speed up builds

--- build.ps1
+++ build.ps1
@@ -1,6 +1,9 @@
 #Disable Telemetry
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
 
+#Skip the NuGet package cache generation
+$env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = 1
+
 function FindProjects($path)
 {
     return gci -Filter project.json -Path $path -Recurse
