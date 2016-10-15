03f568ce9df0df15fe924fa362928b1e3e3fa55e
Stuart Blackler (@Im5tu) <Im5tu@users.noreply.github.com>
2016-10-15 21:05:42 +0100

    Build script improvements

--- build.ps1
+++ build.ps1
@@ -6,6 +6,11 @@ function FindProjects($path)
     return gci -Filter project.json -Path $path -Recurse
 }
 
+function GetProjectName($path)
+{
+    return Split-Path (Split-Path $path -Parent) -Leaf
+}
+
 #Restore packages
 Write-Host "Restoring Packages" -ForegroundColor Green
 . dotnet restore
@@ -13,17 +18,27 @@ Write-Host "Restoring Packages" -ForegroundColor Green
 #Build projects
 function Build ($location)
 {
-    Write-Host "Building project '$location'..." -ForegroundColor Green
+    $project = GetProjectName $location
+    Write-Host "Building project '$project'..." -ForegroundColor Green
     . dotnet build $location
 }
 
-FindProjects "src" | % { Build $_.FullName }
+(Get-Content "global.json" | ConvertFrom-Json).projects | FindProjects $_ | % { Build $_.FullName }
 
 #Test projects
-function Test ($location)
+if((Test-Path "testResults") -eq 0)
 {
-    Write-Host "Testing project '$location'..." -ForegroundColor Green
-    . dotnet test $location
+    $dir = mkdir "testResults"
+}
+else
+{
+    Remove-Item "testResults/*.xml" -Force    
+}
+function Test ($location)
+{  
+    $project = GetProjectName $location
+    Write-Host "Testing project '$project'..." -ForegroundColor Green
+    . dotnet test $location --no-build -xml ("testResults\{0}.TestResults.xml" -f $project) 
 }
 
 FindProjects "tests" | % { Test $_.FullName }
