ccdba9bacf609b506fa7691ec6e25ca0c75e2b72
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2017-03-09 21:37:18 +0000

    First test for memory service extensions

---
+++ tests/OpenMessage.Providers.Memory.Tests/MemoryServiceExtensionTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Xunit;
+
+namespace OpenMessage.Providers.Memory.Tests
+{
+    public class MemoryServiceExtensionTests
+    {
+        public class AddMemoryChannel
+        {
+            [Fact]
+            public void GivenANullServiceCollectionThrowArgumentNullException()
+            {
+                Action act = () => MemoryServiceExtensions.AddMemoryChannel<string>(default(IServiceCollection));
+
+                act.ShouldThrow<ArgumentNullException>();
+            }
+        }
+
+    }
+}
