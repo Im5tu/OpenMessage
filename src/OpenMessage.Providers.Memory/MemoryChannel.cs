0ebfdde1b1495cc62a027936cd17a37b1713843d
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2017-03-09 21:36:05 +0000

    namespace correction

--- src/OpenMessage.Providers.Memory/MemoryChannel.cs
+++ src/OpenMessage.Providers.Memory/MemoryChannel.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace OpenMessage.Providers.Azure
+namespace OpenMessage.Providers.Memory
 {
     internal sealed class MemoryChannel<T> : ManagedObservable<T>, IDispatcher<T>
     {
