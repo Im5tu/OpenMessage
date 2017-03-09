543d038630949a101a60a15c923716c033629b4b
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2017-03-09 20:06:08 +0000

    Added initial project version

---
+++ src/OpenMessage.Providers.Memory/MemoryChannel.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenMessage.Providers.Azure
+{
+    internal sealed class MemoryChannel<T> : ManagedObservable<T>, IDispatcher<T>
+    {
+        private readonly IDispatchInterceptor<T>[] _dispatchInterceptors;
+
+        public MemoryChannel(ILogger<ManagedObservable<T>> logger, IEnumerable<IDispatchInterceptor<T>> dispatchInterceptors) 
+            : base(logger)
+        {
+            if (dispatchInterceptors == null)
+                throw new ArgumentNullException(nameof(dispatchInterceptors));
+
+            _dispatchInterceptors = dispatchInterceptors.ToArray();
+        }
+
+        public Task DispatchAsync(T entity, TimeSpan scheduleIn)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            if (scheduleIn < TimeSpan.Zero)
+                throw new ArgumentException("You cannot schedule a message in the past.");
+
+            if (_dispatchInterceptors.Length > 0 
+                && _dispatchInterceptors.Any(interceptor => !interceptor.Intercept(entity)))
+            {
+                var errorTask = new TaskCompletionSource<T>();
+                errorTask.SetException(new Exception("One or more interceptors blocked the dispatching of the message"));
+                return errorTask.Task;
+            }
+
+            return Task.Run(async () =>
+            {
+                if (scheduleIn > TimeSpan.Zero)
+                    await Task.Delay(scheduleIn);
+
+                Notify(entity);
+            });
+        }
+    }
+}
---
+++ src/OpenMessage.Providers.Memory/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using OpenMessage.Providers.Azure;
+using System;
+
+namespace OpenMessage
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddMemoryChannel<T>(this IServiceCollection services)
+        {
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
+
+            services.TryAddScoped(typeof(MemoryChannel<>), typeof(MemoryChannel<>));
+
+            return services.AddBroker<T>().AddScoped<IDispatcher<T>, MemoryChannel<T>>().AddScoped<IObservable<T>, MemoryChannel<T>>();
+        }
+    }
+}
