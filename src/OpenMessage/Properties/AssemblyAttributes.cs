a386ed85eed890cef16b278a5e7250ae6b3e88a5
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2017-03-09 19:54:55 +0000

    Updating all the csprojs to latest version

---
+++ src/OpenMessage/Properties/AssemblyAttributes.cs
@@ -0,0 +1,7 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("DynamicGenProxy2")]
+[assembly: InternalsVisibleTo("OpenMessage.Providers.Azure")]
+[assembly: InternalsVisibleTo("OpenMessage.Providers.Azure.Tests")]
+[assembly: InternalsVisibleTo("OpenMessage.Providers.Memory")]
+[assembly: InternalsVisibleTo("OpenMessage.Tests")]
