d78487a869c62f2479138013911fd00c7888777d
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2017-03-11 15:41:14 +0000

    Exposed internals to memory tests

--- src/OpenMessage/Properties/AssemblyAttributes.cs
+++ src/OpenMessage/Properties/AssemblyAttributes.cs
@@ -4,4 +4,5 @@
 [assembly: InternalsVisibleTo("OpenMessage.Providers.Azure")]
 [assembly: InternalsVisibleTo("OpenMessage.Providers.Azure.Tests")]
 [assembly: InternalsVisibleTo("OpenMessage.Providers.Memory")]
+[assembly: InternalsVisibleTo("OpenMessage.Providers.Memory.Tests")]
 [assembly: InternalsVisibleTo("OpenMessage.Tests")]
