d9fec83692db0dfc91887168e1885680d8ff398f
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2017-03-16 21:18:16 +0000

    Added service extension tests

---
+++ tests/OpenMessage.Tests/ServiceExtensionTests.cs
@@ -0,0 +1,81 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace OpenMessage.Tests
+{
+    public class ServiceExtensionTests
+    {
+        public class AddObserver
+        {
+            [Fact]
+            public void GivenANullServiceCollectionThrowArgumentNullException()
+            {
+                Action act = () => ServiceExtensions.AddObserver<string>(null, str => { });
+
+                act.ShouldThrow<ArgumentNullException>();
+            }
+
+            [Fact]
+            public void GivenANullActionThrowArgumentNullException()
+            {
+                Action act = () => new ServiceCollection().AddObserver<string>(null);
+
+                act.ShouldThrow<ArgumentNullException>();
+            }
+
+            [Fact]
+            public void GivenAnActionThenRegistersObserverInServiceCollection()
+            {
+                var services = new ServiceCollection().AddObserver<string>(str => { });
+
+                services.Any(x => x.ServiceType == typeof(IObserver<string>)).Should().BeTrue();
+            }
+        }
+
+        public class AddBroker
+        {
+            [Fact]
+            public void GivenANullServiceCollectionThrowArgumentNullException()
+            {
+                Action act = () => ServiceExtensions.AddBroker<string>(null);
+
+                act.ShouldThrow<ArgumentNullException>();
+            }
+
+            [Fact]
+            public void GivenASingleInvocationThenAddsBrokerHost()
+            {
+                var services = new ServiceCollection().AddBroker<string>();
+
+                services.Any(x => x.ServiceType == typeof(IBrokerHost)).Should().BeTrue();
+            }
+
+            [Fact]
+            public void GivenMultipleInvocationsThenAddsSingleBrokerHost()
+            {
+                var services = new ServiceCollection().AddBroker<string>().AddBroker<string>();
+
+                services.Count(x => x.ServiceType == typeof(IBrokerHost)).Should().Be(1);
+            }
+
+            [Fact]
+            public void GivenASingleInvocationThenAddsBroker()
+            {
+                var services = new ServiceCollection().AddBroker<string>();
+
+                services.Any(x => x.ServiceType == typeof(IBroker)).Should().BeTrue();
+            }
+
+            [Fact]
+            public void GivenMultipleInvocationsThenAddsSingleBroker()
+            {
+                var services = new ServiceCollection().AddBroker<string>().AddBroker<string>();
+
+                services.Count(x => x.ServiceType == typeof(IBroker)).Should().Be(1);
+            }
+        }
+    }
+}
