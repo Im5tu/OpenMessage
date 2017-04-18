dbbcc4f43ed984bb3583dd8be0b9ce5d6b04f3a3
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2017-04-18 22:14:33 +0100

    internals for specs

--- src/OpenMessage/Properties/AssemblyAttributes.cs
+++ src/OpenMessage/Properties/AssemblyAttributes.cs
@@ -5,4 +5,5 @@
 [assembly: InternalsVisibleTo("OpenMessage.Providers.Azure.Tests")]
 [assembly: InternalsVisibleTo("OpenMessage.Providers.Memory")]
 [assembly: InternalsVisibleTo("OpenMessage.Providers.Memory.Tests")]
+[assembly: InternalsVisibleTo("OpenMessage.Providers.TestSpecifications")]
 [assembly: InternalsVisibleTo("OpenMessage.Tests")]
