d710b9ebd499974d61a87840fd851f8bedea2c11
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2017-04-19 08:24:50 +0100

    added additional tests for azure service bus observables

---
+++ tests/OpenMessage.Providers.Azure.Tests/Observables/QueueObservableTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OpenMessage.Providers.Azure.Configuration;
+using OpenMessage.Providers.TestSpecifications;
+
+namespace OpenMessage.Providers.Azure.Tests.Observables
+{
+    public class QueueObservableTests : ObservableTests<string>
+    {
+        protected override IServiceCollection ConfigureServices(IServiceCollection services)
+            => services.AddOpenMessage().AddQueueObservable<string>().Configure<OpenMessageAzureProviderOptions<string>>(options => options.ConnectionString = "test").AddJsonNetSerializer();
+    }
+}
