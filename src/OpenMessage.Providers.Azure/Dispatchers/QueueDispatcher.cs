c6c3f5653c2fd8839c145b3208c723b19938068d
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2017-04-19 08:56:02 +0100

    Added base dispatcher class

--- src/OpenMessage.Providers.Azure/Dispatchers/QueueDispatcher.cs
+++ src/OpenMessage.Providers.Azure/Dispatchers/QueueDispatcher.cs
@@ -1,46 +1,29 @@
-﻿using OpenMessage.Providers.Azure.Management;
+﻿using OpenMessage.Dispatching;
+using OpenMessage.Providers.Azure.Management;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace OpenMessage.Providers.Azure.Dispatchers
 {
-    internal sealed class QueueDispatcher<T> : IDispatcher<T>
+    internal sealed class QueueDispatcher<T> : DispatcherBase<T>
     {
         private readonly IQueueClient<T> _client;
-        private readonly IDispatchInterceptor<T>[] _dispatchInterceptors;
 
         public QueueDispatcher(IQueueFactory<T> queueFactory,
-            IEnumerable<IDispatchInterceptor<T>> dispatchInterceptors)
+            IEnumerable<IDispatchInterceptor<T>> dispatchInterceptors) : base(dispatchInterceptors)
         {
             if (queueFactory == null)
                 throw new ArgumentNullException(nameof(queueFactory));
 
-            if (dispatchInterceptors == null)
-                throw new ArgumentNullException(nameof(dispatchInterceptors));
-
             _client = queueFactory.Create();
-            _dispatchInterceptors = dispatchInterceptors.ToArray();
         }
 
-        public Task DispatchAsync(T entity, TimeSpan scheduleIn)
+        protected override Task OnDispatchMessageAsync(T entity, TimeSpan scheduleIn) => _client.SendAsync(entity, scheduleIn);
+
+        protected override void OnDispose()
         {
-            if (entity == null)
-                throw new ArgumentNullException(nameof(entity));
-
-            if (scheduleIn < TimeSpan.Zero)
-                throw new ArgumentException("You cannot schedule a message in the past.");
-
-            if (_dispatchInterceptors.Length > 0
-                    && _dispatchInterceptors.Any(interceptor => !interceptor.Intercept(entity)))
-            {
-                var errorTask = new TaskCompletionSource<T>();
-                errorTask.SetException(new Exception("One or more interceptors blocked the dispatching of the message"));
-                return errorTask.Task;
-            }
-
-            return _client.SendAsync(entity, scheduleIn);
+            _client?.Dispose();
         }
     }
 }
---
+++ src/OpenMessage/Dispatching/DispatcherBase.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenMessage.Dispatching
+{
+    public abstract class DispatcherBase<T> : IDispatcher<T>, IDisposable
+    {
+        private bool _isDisposed = false;
+        private readonly IDispatchInterceptor<T>[] _dispatchInterceptors;
+
+        public DispatcherBase(IEnumerable<IDispatchInterceptor<T>> dispatchInterceptors)
+        {
+            if (dispatchInterceptors == null)
+                throw new ArgumentNullException(nameof(dispatchInterceptors));
+
+            _dispatchInterceptors = dispatchInterceptors.ToArray();
+        }
+
+        public Task DispatchAsync(T entity, TimeSpan scheduleIn)
+        {
+            ThrowIfDisposed();
+
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            if (scheduleIn < TimeSpan.Zero)
+                throw new ArgumentNullException(nameof(scheduleIn));
+
+            if (_dispatchInterceptors.Length > 0
+                    && _dispatchInterceptors.Any(interceptor => interceptor.Intercept(entity)))
+            {
+                var errorTask = new TaskCompletionSource<T>();
+                errorTask.SetException(new Exception("One or more interceptors blocked the dispatching of the message"));
+                return errorTask.Task;
+            }
+
+            return OnDispatchMessageAsync(entity, scheduleIn);
+        }
+
+        protected abstract Task OnDispatchMessageAsync(T entity, TimeSpan scheduleIn);
+
+        public void Dispose()
+        {
+            ThrowIfDisposed();
+
+            _isDisposed = true;
+
+            foreach (var disposableDispatchInterceptor in _dispatchInterceptors.OfType<IDisposable>())
+                disposableDispatchInterceptor.Dispose();
+
+            OnDispose();
+        }
+        protected virtual void OnDispose() { }
+        protected void ThrowIfDisposed()
+        {
+            if (_isDisposed)
+                throw new ObjectDisposedException(GetType().Name);
+        }
+    }
+}
