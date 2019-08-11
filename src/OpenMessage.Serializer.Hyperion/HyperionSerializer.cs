867a4ffc691b7e4f87ae6c72886f707524cfb260
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2019-08-11 18:20:10 +0100

    Added the hyperion serializer & deserializer

--- src/OpenMessage.Serializer.Hyperion/HyperionSerializer.cs
+++ src/OpenMessage.Serializer.Hyperion/HyperionSerializer.cs
@@ -1,7 +1,52 @@
-﻿namespace OpenMessage.Serializer.Hyperion
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Hyperion;
+using OpenMessage.Extensions;
+using OpenMessage.Serialisation;
+
+namespace OpenMessage.Serializer.Hyperion
 {
-    public class HyperionSerializer
+    using Serializer = global::Hyperion.Serializer;
+    // TODO :: Expose settings via options
+
+    internal sealed class HyperionSerializer : ISerializer, IDeserializer
     {
-        
+        private static readonly string _contentType = "binary/hyperion";
+        private static readonly Serializer _serialiser = new Serializer(new SerializerOptions(preserveObjectReferences: true));
+
+        public string ContentType { get; } = _contentType;
+        public IEnumerable<string> SupportedContentTypes { get; } = new[] {_contentType};
+
+        public string AsString<T>(T entity)
+        {
+            entity.Must(nameof(entity)).NotBeNull();
+
+            return Convert.ToBase64String(AsBytes(entity));
+        }
+
+        public byte[] AsBytes<T>(T entity)
+        {
+            entity.Must(nameof(entity)).NotBeNull();
+
+            using var ms = new MemoryStream();
+            _serialiser.Serialize(entity, ms);
+            return ms.ToArray();
+        }
+
+        public T From<T>(string data)
+        {
+            data.Must(nameof(data)).NotBeNullOrWhiteSpace();
+
+            return From<T>(Convert.FromBase64String(data));
+        }
+
+        public T From<T>(byte[] data)
+        {
+            data.Must(nameof(data)).NotBeNullOrEmpty();
+
+            using var ms = new MemoryStream(data);
+            return _serialiser.Deserialize<T>(ms);
+        }
     }
 }
--- src/OpenMessage.Serializer.Hyperion/HyperionSerializerServiceExtensions.cs
+++ src/OpenMessage.Serializer.Hyperion/HyperionSerializerServiceExtensions.cs
@@ -1,7 +1,46 @@
-﻿namespace OpenMessage.Serializer.Hyperion
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using OpenMessage.Serialisation;
+
+namespace OpenMessage.Serializer.Hyperion
 {
-    public class HyperionSerializerServiceExtensions
+    /// <summary>
+    /// Hyperion Service Extensions
+    /// </summary>
+    public static class HyperionSerializerServiceExtensions
     {
-        
+        /// <summary>
+        /// Adds the hyperion serializer & deserializer
+        /// </summary>
+        /// <param name="messagingBuilder">The host to configure</param>
+        /// <returns>The modified builder</returns>
+        public static IMessagingBuilder ConfigureHyperion(this IMessagingBuilder messagingBuilder)
+        {
+            return messagingBuilder.ConfigureHyperionDeserializer().ConfigureHyperionSerializer();
+        }
+
+        /// <summary>
+        /// Adds the hyperion serializer
+        /// </summary>
+        /// <param name="messagingBuilder">The host to configure</param>
+        /// <returns>The modified builder</returns>
+        public static IMessagingBuilder ConfigureHyperionSerializer(this IMessagingBuilder messagingBuilder)
+        {
+            messagingBuilder.Services.TryAddSingleton<HyperionSerializer>();
+            messagingBuilder.Services.AddSingleton<ISerializer>(sp => sp.GetRequiredService<HyperionSerializer>());
+            return messagingBuilder;
+        }
+
+        /// <summary>
+        /// Adds the hyperion deserializer
+        /// </summary>
+        /// <param name="messagingBuilder">The host to configure</param>
+        /// <returns>The modified builder</returns>
+        public static IMessagingBuilder ConfigureHyperionDeserializer(this IMessagingBuilder messagingBuilder)
+        {
+            messagingBuilder.Services.TryAddSingleton<HyperionSerializer>();
+            messagingBuilder.Services.AddSingleton<IDeserializer>(sp => sp.GetRequiredService<HyperionSerializer>());
+            return messagingBuilder;
+        }
     }
 }
