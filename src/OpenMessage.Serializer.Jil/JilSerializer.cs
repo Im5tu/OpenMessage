5dfdc63702e87b501a5270eafac3224d67968212
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2019-08-11 18:31:11 +0100

    Added Jil Serializer

---
+++ src/OpenMessage.Serializer.Jil/JilSerializer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Jil;
+using OpenMessage.Extensions;
+using OpenMessage.Serialisation;
+
+namespace OpenMessage.Serializer.Jil
+{
+    internal sealed class JilSerializer : ISerializer, IDeserializer
+    {
+        private static readonly string _contentType = "binary/hyperion";
+
+        public string ContentType { get; } = _contentType;
+        public IEnumerable<string> SupportedContentTypes { get; } = new[] {_contentType};
+
+        public string AsString<T>(T entity)
+        {
+            entity.Must(nameof(entity)).NotBeNull();
+
+            return JSON.Serialize(entity);
+        }
+
+        public byte[] AsBytes<T>(T entity)
+        {
+            entity.Must(nameof(entity)).NotBeNull();
+
+            return Encoding.UTF8.GetBytes(JSON.Serialize(entity));
+        }
+
+        public T From<T>(string data)
+        {
+            data.Must(nameof(data)).NotBeNullOrWhiteSpace();
+
+            return JSON.Deserialize<T>(data);
+        }
+
+        public T From<T>(byte[] data)
+        {
+            data.Must(nameof(data)).NotBeNullOrEmpty();
+
+            return JSON.Deserialize<T>(Encoding.UTF8.GetString(data));
+        }
+    }
+}
---
+++ src/OpenMessage.Serializer.Jil/JilServiceExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using OpenMessage.Serialisation;
+
+namespace OpenMessage.Serializer.Jil
+{
+    /// <summary>
+    ///     Jil Service Extensions
+    /// </summary>
+    public static class JilSerializerServiceExtensions
+    {
+        /// <summary>
+        ///     Adds the Jil serializer & deserializer
+        /// </summary>
+        /// <param name="messagingBuilder">The host to configure</param>
+        /// <returns>The modified builder</returns>
+        public static IMessagingBuilder ConfigureJil(this IMessagingBuilder messagingBuilder)
+        {
+            return messagingBuilder.ConfigureJilDeserializer().ConfigureJilSerializer();
+        }
+
+        /// <summary>
+        ///     Adds the Jil serializer
+        /// </summary>
+        /// <param name="messagingBuilder">The host to configure</param>
+        /// <returns>The modified builder</returns>
+        public static IMessagingBuilder ConfigureJilSerializer(this IMessagingBuilder messagingBuilder)
+        {
+            messagingBuilder.Services.TryAddSingleton<JilSerializer>();
+            messagingBuilder.Services.AddSerialization().AddSingleton<ISerializer>(sp => sp.GetRequiredService<JilSerializer>());
+            return messagingBuilder;
+        }
+
+        /// <summary>
+        ///     Adds the Jil deserializer
+        /// </summary>
+        /// <param name="messagingBuilder">The host to configure</param>
+        /// <returns>The modified builder</returns>
+        public static IMessagingBuilder ConfigureJilDeserializer(this IMessagingBuilder messagingBuilder)
+        {
+            messagingBuilder.Services.TryAddSingleton<JilSerializer>();
+            messagingBuilder.Services.AddSerialization().AddSingleton<IDeserializer>(sp => sp.GetRequiredService<JilSerializer>());
+            return messagingBuilder;
+        }
+    }
+}
