807b1bb9ce45d64036dafba149b030dfe3d32b8b
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2019-08-11 20:26:53 +0100

    Added MsgPack serializer

---
+++ src/OpenMessage.Serializer.MsgPackCli/MsgPackSerializer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using MsgPack.Serialization;
+using OpenMessage.Extensions;
+using OpenMessage.Serialisation;
+
+namespace OpenMessage.Serializer.MsgPackCli
+{
+    internal sealed class MsgPackSerializer : ISerializer, IDeserializer
+    {
+        private static readonly string _contentType = "binary/msgpack";
+        private readonly ConcurrentDictionary<Type, MessagePackSerializer> _serialisers = new ConcurrentDictionary<Type, MessagePackSerializer>();
+
+        public string ContentType { get; } = _contentType;
+        public IEnumerable<string> SupportedContentTypes { get; } = new[] {_contentType};
+
+        public string AsString<T>(T entity)
+        {
+            entity.Must(nameof(entity)).NotBeNull();
+
+            return Convert.ToBase64String(AsBytes(entity));
+        }
+
+        public byte[] AsBytes<T>(T entity)
+        {
+            entity.Must(nameof(entity)).NotBeNull();
+
+            return _serialisers.GetOrAdd(typeof(T), key => MessagePackSerializer.Get(key)).PackSingleObject(entity);
+        }
+
+        public T From<T>(string data)
+        {
+            data.Must(nameof(data)).NotBeNullOrWhiteSpace();
+
+            return From<T>(Convert.FromBase64String(data));
+        }
+
+        public T From<T>(byte[] data)
+        {
+            data.Must(nameof(data)).NotBeNullOrEmpty();
+
+            return (T) _serialisers.GetOrAdd(typeof(T), key => MessagePackSerializer.Get(key)).UnpackSingleObject(data);
+        }
+    }
+}
---
+++ src/OpenMessage.Serializer.MsgPackCli/MsgPackServiceExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using OpenMessage.Serialisation;
+
+namespace OpenMessage.Serializer.MsgPackCli
+{
+    /// <summary>
+    ///     MsgPack Service Extensions
+    /// </summary>
+    public static class MsgPackSerializerServiceExtensions
+    {
+        /// <summary>
+        ///     Adds the MsgPack serializer &amp; deserializer
+        /// </summary>
+        /// <param name="messagingBuilder">The host to configure</param>
+        /// <returns>The modified builder</returns>
+        public static IMessagingBuilder ConfigureMsgPack(this IMessagingBuilder messagingBuilder)
+        {
+            return messagingBuilder.ConfigureMsgPackDeserializer().ConfigureMsgPackSerializer();
+        }
+
+        /// <summary>
+        ///     Adds the MsgPack serializer
+        /// </summary>
+        /// <param name="messagingBuilder">The host to configure</param>
+        /// <returns>The modified builder</returns>
+        public static IMessagingBuilder ConfigureMsgPackSerializer(this IMessagingBuilder messagingBuilder)
+        {
+            messagingBuilder.Services.TryAddSingleton<MsgPackSerializer>();
+            messagingBuilder.Services.AddSerialization().AddSingleton<ISerializer>(sp => sp.GetRequiredService<MsgPackSerializer>());
+            return messagingBuilder;
+        }
+
+        /// <summary>
+        ///     Adds the MsgPack deserializer
+        /// </summary>
+        /// <param name="messagingBuilder">The host to configure</param>
+        /// <returns>The modified builder</returns>
+        public static IMessagingBuilder ConfigureMsgPackDeserializer(this IMessagingBuilder messagingBuilder)
+        {
+            messagingBuilder.Services.TryAddSingleton<MsgPackSerializer>();
+            messagingBuilder.Services.AddSerialization().AddSingleton<IDeserializer>(sp => sp.GetRequiredService<MsgPackSerializer>());
+            return messagingBuilder;
+        }
+    }
+}
