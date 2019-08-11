4ab7b2a0ee6c3cda619fd8d17588681336d5fa81
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2019-08-11 20:27:09 +0100

    Added protobuf serializer

---
+++ src/OpenMessage.Serializer.Protobuf/ProtobufSerializer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using OpenMessage.Extensions;
+using OpenMessage.Serialisation;
+
+namespace OpenMessage.Serializer.Protobuf
+{
+    using Serializer = Protobuf;
+
+    internal sealed class ProtobufSerializer : ISerializer, IDeserializer
+    {
+        private static readonly string _contentType = "binary/protobuf";
+
+        public string ContentType { get; } = _contentType;
+        public IEnumerable<string> SupportedContentTypes { get; } = new[] {_contentType};
+
+        public string AsString<T>(T entity)
+        {
+            entity.Must(nameof(entity)).NotBeNull();
+
+            return Convert.ToBase64String(AsBytes(entity));
+        }
+
+        public byte[] AsBytes<T>(T entity)
+        {
+            entity.Must(nameof(entity)).NotBeNull();
+
+            using var ms = new MemoryStream();
+            ProtoBuf.Serializer.Serialize(ms, entity);
+            return ms.ToArray();
+        }
+
+        public T From<T>(string data)
+        {
+            data.Must(nameof(data)).NotBeNullOrWhiteSpace();
+
+            return From<T>(Convert.FromBase64String(data));
+        }
+
+        public T From<T>(byte[] data)
+        {
+            data.Must(nameof(data)).NotBeNullOrEmpty();
+
+            using var ms = new MemoryStream(data);
+            return ProtoBuf.Serializer.Deserialize<T>(ms);
+        }
+    }
+}
---
+++ src/OpenMessage.Serializer.Protobuf/ProtobufServiceExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using OpenMessage.Serialisation;
+
+namespace OpenMessage.Serializer.Protobuf
+{
+    /// <summary>
+    ///     Protobuf Service Extensions
+    /// </summary>
+    public static class ProtobufSerializerServiceExtensions
+    {
+        /// <summary>
+        ///     Adds the Protobuf serializer &amp; deserializer
+        /// </summary>
+        /// <param name="messagingBuilder">The host to configure</param>
+        /// <returns>The modified builder</returns>
+        public static IMessagingBuilder ConfigureProtobuf(this IMessagingBuilder messagingBuilder)
+        {
+            return messagingBuilder.ConfigureProtobufDeserializer().ConfigureProtobufSerializer();
+        }
+
+        /// <summary>
+        ///     Adds the Protobuf serializer
+        /// </summary>
+        /// <param name="messagingBuilder">The host to configure</param>
+        /// <returns>The modified builder</returns>
+        public static IMessagingBuilder ConfigureProtobufSerializer(this IMessagingBuilder messagingBuilder)
+        {
+            messagingBuilder.Services.TryAddSingleton<ProtobufSerializer>();
+            messagingBuilder.Services.AddSerialization().AddSingleton<ISerializer>(sp => sp.GetRequiredService<ProtobufSerializer>());
+            return messagingBuilder;
+        }
+
+        /// <summary>
+        ///     Adds the Protobuf deserializer
+        /// </summary>
+        /// <param name="messagingBuilder">The host to configure</param>
+        /// <returns>The modified builder</returns>
+        public static IMessagingBuilder ConfigureProtobufDeserializer(this IMessagingBuilder messagingBuilder)
+        {
+            messagingBuilder.Services.TryAddSingleton<ProtobufSerializer>();
+            messagingBuilder.Services.AddSerialization().AddSingleton<IDeserializer>(sp => sp.GetRequiredService<ProtobufSerializer>());
+            return messagingBuilder;
+        }
+    }
+}
