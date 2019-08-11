17c97b19a7d025b0a62386be2b376bad6c14a9f0
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2019-08-11 20:28:19 +0100

    Added wire serializer

---
+++ src/OpenMessage.Serializer.Wire/WireSerializer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using OpenMessage.Extensions;
+using OpenMessage.Serialisation;
+
+namespace OpenMessage.Serializer.Wire
+{
+    internal sealed class WireSerializer : ISerializer, IDeserializer
+    {
+        private static readonly global::Wire.Serializer _serialiser = new global::Wire.Serializer();
+        private static readonly string _contentType = "binary/wire";
+
+        public string ContentType { get; } = _contentType;
+        public IEnumerable<string> SupportedContentTypes { get; } = new[] {_contentType};
+
+        public string AsString<T>(T entity)
+        {
+            entity.Must(nameof(entity)).NotBeNull();
+
+            return Convert.ToBase64String(AsBytes(entity));
+        }
+
+        public byte[] AsBytes<T>(T entity)
+        {
+            entity.Must(nameof(entity)).NotBeNull();
+
+            using var ms = new MemoryStream();
+            _serialiser.Serialize(entity, ms);
+            return ms.ToArray();
+        }
+
+        public T From<T>(string data)
+        {
+            data.Must(nameof(data)).NotBeNullOrWhiteSpace();
+
+            return From<T>(Convert.FromBase64String(data));
+        }
+
+        public T From<T>(byte[] data)
+        {
+            data.Must(nameof(data)).NotBeNullOrEmpty();
+
+            using var ms = new MemoryStream(data);
+            return _serialiser.Deserialize<T>(ms);
+        }
+    }
+}
---
+++ src/OpenMessage.Serializer.Wire/WireServiceExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using OpenMessage.Serialisation;
+
+namespace OpenMessage.Serializer.Wire
+{
+    /// <summary>
+    ///     Wire Service Extensions
+    /// </summary>
+    public static class WireSerializerServiceExtensions
+    {
+        /// <summary>
+        ///     Adds the Wire serializer & deserializer
+        /// </summary>
+        /// <param name="messagingBuilder">The host to configure</param>
+        /// <returns>The modified builder</returns>
+        public static IMessagingBuilder ConfigureWire(this IMessagingBuilder messagingBuilder)
+        {
+            return messagingBuilder.ConfigureWireDeserializer().ConfigureWireSerializer();
+        }
+
+        /// <summary>
+        ///     Adds the Wire serializer
+        /// </summary>
+        /// <param name="messagingBuilder">The host to configure</param>
+        /// <returns>The modified builder</returns>
+        public static IMessagingBuilder ConfigureWireSerializer(this IMessagingBuilder messagingBuilder)
+        {
+            messagingBuilder.Services.TryAddSingleton<WireSerializer>();
+            messagingBuilder.Services.AddSerialization().AddSingleton<ISerializer>(sp => sp.GetRequiredService<WireSerializer>());
+            return messagingBuilder;
+        }
+
+        /// <summary>
+        ///     Adds the Wire deserializer
+        /// </summary>
+        /// <param name="messagingBuilder">The host to configure</param>
+        /// <returns>The modified builder</returns>
+        public static IMessagingBuilder ConfigureWireDeserializer(this IMessagingBuilder messagingBuilder)
+        {
+            messagingBuilder.Services.TryAddSingleton<WireSerializer>();
+            messagingBuilder.Services.AddSerialization().AddSingleton<IDeserializer>(sp => sp.GetRequiredService<WireSerializer>());
+            return messagingBuilder;
+        }
+    }
+}
