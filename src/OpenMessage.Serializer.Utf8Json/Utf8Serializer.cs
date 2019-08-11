24253dd9b16ca20fcb15604097590317d0f37402
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2019-08-11 20:27:59 +0100

    Added Utf8Json serializer

---
+++ src/OpenMessage.Serializer.Utf8Json/Utf8Serializer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using OpenMessage.Extensions;
+using OpenMessage.Serialisation;
+using Utf8Json;
+
+namespace OpenMessage.Serializer.Utf8Json
+{
+    internal sealed class Utf8Serializer : ISerializer, IDeserializer
+    {
+        private static readonly string _contentType = "application/json";
+
+        public string ContentType { get; } = _contentType;
+        public IEnumerable<string> SupportedContentTypes { get; } = new[] {_contentType};
+
+        public string AsString<T>(T entity)
+        {
+            entity.Must(nameof(entity)).NotBeNull();
+
+            return JsonSerializer.ToJsonString(entity);
+        }
+
+        public byte[] AsBytes<T>(T entity)
+        {
+            entity.Must(nameof(entity)).NotBeNull();
+
+            return JsonSerializer.Serialize(entity);
+        }
+
+        public T From<T>(string data)
+        {
+            data.Must(nameof(data)).NotBeNullOrWhiteSpace();
+
+            return JsonSerializer.Deserialize<T>(data);
+        }
+
+        public T From<T>(byte[] data)
+        {
+            data.Must(nameof(data)).NotBeNullOrEmpty();
+
+            return JsonSerializer.Deserialize<T>(data);
+        }
+    }
+}
