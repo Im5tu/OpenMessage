b9ee94b7e44b09711d8d748be57a016a69477f55
Scott Ngan <scott.ngan@provoke.co.nz>
2019-11-25 09:57:38 +1300

    Revert "Removed old memory implementation"
    This reverts commit f0f57581204a9d5520dbbfafc7a1656f4a184334.

---
+++ src/OpenMessage/Memory/MemoryDispatcher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using OpenMessage.Extensions;
+
+namespace OpenMessage.Memory
+{
+    internal sealed class MemoryDispatcher<T> : IDispatcher<T>
+    {
+        private readonly ChannelWriter<Message<T>> _channelWriter;
+
+        public MemoryDispatcher(ChannelWriter<Message<T>> channelWriter)
+        {
+            _channelWriter = channelWriter ?? throw new ArgumentNullException(nameof(channelWriter));
+        }
+
+        public async Task DispatchAsync(Message<T> entity, CancellationToken cancellationToken)
+        {
+            entity.Must(nameof(entity)).NotBeNull();
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (!await _channelWriter.WaitToWriteAsync(cancellationToken))
+                Throw.Exception("Cannot write to channel");
+
+            await _channelWriter.WriteAsync(entity, cancellationToken);
+        }
+
+        public Task DispatchAsync(T entity, CancellationToken cancellationToken)
+        {
+            return DispatchAsync(new Message<T> {Value = entity}, cancellationToken);
+        }
+    }
+}
---
+++ src/OpenMessage/Memory/MemoryProviderExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Channels;
+using OpenMessage;
+using OpenMessage.Extensions;
+using OpenMessage.Memory;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    ///     Extension methods for adding an InMemory Dispatcher/Consumer
+    /// </summary>
+    public static class MemoryProviderExtensions
+    {
+        /// <summary>
+        ///     Adds an InMemory dispatcher
+        /// </summary>
+        /// <param name="builder">The current host builder</param>
+        /// <param name="channelCreator">The function to use to create the underlying channel</param>
+        /// <typeparam name="T">The type to handle</typeparam>
+        /// <returns>An instance of <see cref="IMessagingBuilder" /></returns>
+        public static IMessagingBuilder ConfigureMemoryDispatcher<T>(this IMessagingBuilder builder, Func<IServiceProvider, Channel<Message<T>>> channelCreator = null)
+        {
+            builder.Services.TryAddChannel(channelCreator).AddSingleton<IDispatcher<T>, MemoryDispatcher<T>>();
+            return builder;
+        }
+
+        /// <summary>
+        ///     Adds an InMemory consumer
+        /// </summary>
+        /// <param name="builder">The current host builder</param>
+        /// <param name="channelCreator">The function to use to create the underlying channel</param>
+        /// <typeparam name="T">The type to handle</typeparam>
+        /// <returns>An instance of <see cref="IMessagingBuilder" /></returns>
+        public static IMessagingBuilder ConfigureMemoryConsumer<T>(this IMessagingBuilder builder, Func<IServiceProvider, Channel<Message<T>>> channelCreator = null)
+        {
+            builder.Services.TryAddConsumerService<T>(channelCreator);
+            return builder;
+        }
+
+        /// <summary>
+        ///     Adds an InMemory consumer and dispatcher
+        /// </summary>
+        /// <param name="builder">The current host builder</param>
+        /// <param name="channelCreator">The function to use to create the underlying channel</param>
+        /// <typeparam name="T">The type to handle</typeparam>
+        /// <returns>An instance of <see cref="IMessagingBuilder" /></returns>
+        public static IMessagingBuilder ConfigureMemory<T>(this IMessagingBuilder builder, Func<IServiceProvider, Channel<Message<T>>> channelCreator = null)
+        {
+            return builder.ConfigureMemoryConsumer(channelCreator).ConfigureMemoryDispatcher(channelCreator);
+        }
+    }
+}
